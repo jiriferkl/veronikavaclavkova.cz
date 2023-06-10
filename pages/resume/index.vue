362c75d4fefff53cca9bc9a0c619aa23f9c47b8b
Jiří Ferkl <jiri.ferkl@protonmail.com>
2023-06-11 01:43:57 +0200

    App: resume.pdf added

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -3,7 +3,7 @@
     <ResumeComponent>
       <template v-slot:top>
         <div class="absolute right-4 top-2">
-          <a href="https://pdf.veronikavaclavkova.cz/" target="_blank" title="Download me!" class="mr-1.5"><font-awesome-icon icon="fa-solid fa-download" /></a>
+          <a href="/resume.pdf" target="_blank" title="Download me!" class="mr-1.5"><font-awesome-icon icon="fa-solid fa-download" /></a>
           <a href="/resume/print" target="_blank" title="Print me!"><font-awesome-icon icon="fa-solid fa-print" /></a>
         </div>
       </template>
