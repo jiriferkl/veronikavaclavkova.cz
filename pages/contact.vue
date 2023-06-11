94f7e9b7f8cb1daa8ecf7b154851a4ba76988cd6
Jiří Ferkl <jiri.ferkl@protonmail.com>
2023-06-11 12:25:04 +0200

    App: contact styles

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-10 flex flex-col items-center bg-white text-black">
-    <h1 class="text-4xl mb-10 mt-20">Ahoj!</h1>
+    <h1 class="text-4xl mb-10 mt-10">Ahoj!</h1>
     <iframe class="max-w-full" src="https://giphy.com/embed/QLKSt3wQqlj7a" width="480"></iframe>
     <p class="mt-10 text-xl text-center">Nyní Vám jen řeknu, že bydlím v Pardubicích.</p>
     <p class="mt-8 text-xl text-center">Pokud mě chcete více poznat, neváhejte mě kontaktovat.</p>
