c80b65e0dfb42195dee35b4634daac016bab42d0
Jiří Ferkl <jiri.ferkl@protonmail.com>
2023-06-11 12:28:51 +0200

    App: added meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,20 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: "Veronika Václavková - Grafický designer"
+      title: "Veronika Václavková - Grafický designer",
+      meta: [
+        { name: 'description', content: 'Ráda příjmu nové výzvy! 3DS Max, Unity, Substance 3D Painter, Photoshop, Illustrator, Figma..' },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:url', content: 'https://veronikavaclavkova.cz/' },
+        { name: 'og:title', content: 'Veronika Václavková - Grafický designer' },
+        { name: 'og:description', content: 'Ráda příjmu nové výzvy! 3DS Max, Unity, Substance 3D Painter, Photoshop, Illustrator, Figma..' },
+        { name: 'og:image', content: 'https://veronikavaclavkova.cz/ogimage.png' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://veronikavaclavkova.cz/' },
+        { name: 'twitter:title', content: 'Veronika Václavková - Grafický designer' },
+        { name: 'twitter:description', content: 'Ráda příjmu nové výzvy! 3DS Max, Unity, Substance 3D Painter, Photoshop, Illustrator, Figma..' },
+        { name: 'twitter:image', content: 'https://veronikavaclavkova.cz/ogimage.png' },
+      ],
     },
   },
 })
