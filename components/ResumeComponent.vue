a6431a2c1b5e4184409e3cda3af84825fbcf48f6
Jiří Ferkl <jiri.ferkl@protonmail.com>
2023-06-11 12:08:43 +0200

    App: resume performance

--- components/ResumeComponent.vue
+++ components/ResumeComponent.vue
@@ -107,14 +107,97 @@
 
 <script>
 export default defineNuxtComponent({
-  async asyncData() {
-    return {
-      experience: await $fetch('/api/experience'),
-      education: await $fetch('/api/education')
-    }
-  },
   data() {
     return {
+      experience: [
+        {
+          "position": "Člen marketingového týmu",
+          "years": "8.2021 - současnost",
+          "place": "Pardubice",
+          "url": "https://www.dfmarketing.cz/",
+          "company": "DF marketing",
+          "description": [
+            "Ve firmě DF Marketing jsem byla členem marketingového týmu. Podílela jsem se na velké části celého procesu od statistik, korektur článků až po SEO.",
+          ],
+          "badges": [],
+          "logo": "/companies/df.png"
+        },
+        {
+          "position": "Grafik Junior",
+          "years": "1.2015 - současnost",
+          "place": "Pardubice",
+          "company": "",
+          "description": [
+            "Už od roku 2015 tvořím loga a plakáty, návrhy webů a jiné grafické práce.",
+          ],
+          "badges": [],
+          "logo": "/companies/palette.svg",
+        },
+        {
+          "position": "Uvaděčka",
+          "years": "9.2019 - 7.2021",
+          "place": "Pardubice",
+          "url": "https://www.vcd.cz/",
+          "company": "Východočeské divadlo Pardubice",
+          "description": [
+            "Ve východočeském divadle Pardubice jsem pracovala jako uvaděčka a šatnářka 2 divadelní sezóny.",
+          ],
+          "badges": [],
+          "logo": "/companies/vcd.jpg"
+        },
+        {
+          "position": "Brigády",
+          "years": "1.2015 - 12.2021",
+          "place": "Žamberk, Klášterec nad Orlicí, Hylváty",
+          "company": "Různé",
+          "description": [
+            "Táborová vedoucí, zdravotní sestra v zubní ordinaci a další menší brigády.",
+            "Jako zdravotní sestra jsem vypomáhala v zubní ordinaci doktorky MDDr. Simony Pavlové.",
+            "Od roku 2019 do roku 2021 jsem dělala táborovou vedoucí na příměstském táboře Animo v Klášterci nad Orlicí a také na příměstském táboře Jezdecká škola Hylváty",
+          ],
+          "badges": [],
+          "logo": null,
+        },
+      ],
+      education: [
+        {
+          "years": "2019 - 2023",
+          "place": "Pardubice",
+          "url": "http://spse.cz/",
+          "title": "Střední průmyslová škola elektrotechnická a Vyšší odborná škola Pardubice",
+          "subtitle": "Grafika a webdesing",
+          "description": [
+            "Během studia na vysoké škole jsem začala navštěvovat střední školu s oborem zaměřeným na grafiku a webdesign."
+          ],
+          "badges": [],
+          "logo": "/schools/spse.png"
+        },
+        {
+          "years": "2017 - 2021",
+          "place": "Opava",
+          "url": "http://www.slu.cz",
+          "title": "Slezská univerzita v Opavě",
+          "subtitle": "Multimediální techniky",
+          "description": [
+            "Už během studia střední školy jsem zjistila, že se chci vydat jiným směrem. Na vysoké škole jsem se zaměřila na grafický design a práci produkční.",
+            "Během tohoto studia jsem získala velké zkušenosti na několika projekech. Největší zkušeností byla práce na dokumentu Perkův dalekohled a nebo výhra v soutěži 48 Hour Film Project Brno 2018. Tento vítězný snímek dále postoupil na filmový festival do Orlanda, kterého jsem se zúčastnila.",
+          ],
+          "badges": [],
+          "logo": "/schools/slu.jpg"
+        },
+        {
+          "years": "2013 - 2017",
+          "place": "Ústí nad Orlicí",
+          "url": "https://www.szsuo.cz/",
+          "title": "Vyšší odborná škola a střední škola zdravotnická a sociální Ústí nad Orlicí",
+          "subtitle": "Zdravotnický asistent",
+          "description": [
+            "Po základní škole jsem se rozhodla nastoupit na zdravotní školu, abych se stala zdravotní sestrou.",
+          ],
+          "badges": [],
+          "logo": "/schools/szs.png",
+        },
+      ],
       contactInfo: [
         {
           icon: "fa-solid fa-paperclip",
