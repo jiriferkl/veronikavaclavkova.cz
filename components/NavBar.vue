02fcc61e514287b34059292cd5391b9a746e9a0d
Jiří Ferkl <jiri.ferkl@protonmail.com>
2023-10-01 13:32:53 +0200

    App: navbar fix closing

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="p-4 px-10 z-10">
     <div class="flex flex-row justify-between items-center mb-1">
-      <NuxtLink to="/"><img src="/logo.svg" alt="logo" class="h-12"></NuxtLink>
-      <Nav class="hidden sm:block"/>
+      <NuxtLink @click="toggle = false" to="/"><img src="/logo.svg" alt="logo" class="h-12"></NuxtLink>
+      <Nav @click="toggle = false" class="hidden sm:block"/>
       <a @click.prevent="toggle = !toggle" class="block sm:hidden cursor-pointer"><font-awesome-icon icon="fa-solid fa-bars"/></a>
     </div>
     <Transition>
-      <Nav v-show="toggle" class="sm:hidden flex flex-col overflow-hidden" />
+      <Nav @click="toggle = false" v-show="toggle" class="sm:hidden flex flex-col overflow-hidden" />
     </Transition>
   </div>
 </template>
