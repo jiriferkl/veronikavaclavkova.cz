079fd936dae17a2448d92ed7785925511a7e8aa2
Jiří Ferkl <jiri.ferkl@protonmail.com>
2023-10-01 14:23:55 +0200

    App: added content to 2d portfolio

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -6,6 +6,13 @@
 
       <div v-for="(subgallery, subgalleryKey) in portfolio.subGalleries" :key="subgalleryKey">
         <h3 class="text-2xl mt-5 mb-2 text-center sm:text-left">{{ subgallery.title }}</h3>
+        <h3 class="text-xl text-center sm:text-left">{{ subgallery.subtitle }}</h3>
+        <div v-if="subgallery.info" class="flex my-2 bg-blue-50 p-3 text-blue-900 rounded w-fit text-sm sm:text-base">
+          <div class="mr-1">
+            <font-awesome-icon icon="fa-solid fa-circle-info" />
+          </div>
+          <div v-html="subgallery.info"></div>
+        </div>
         <gallery :gallery-i-d="subgallery.id" :images="subgallery.images"></gallery>
       </div>
     </div>
@@ -19,8 +26,172 @@ export default defineNuxtComponent({
       portfolio: {
         subGalleries: [
           {
-            id: "gallery2d-materials",
+            id: "gallery2d-tauslova",
             title: "Propagační materiály",
+            subtitle: "Mateřská škola Taušlova",
+            info: "<a href='https://www.tauslovka.cz/' target='_blank'>https://www.tauslovka.cz/</a>",
+            images: [
+              {
+                "src": "/portfolio/2d/propagacni_materialy/5800894.jpg",
+                "width": 7162,
+                "height": 4780
+              },
+            ],
+          },
+          {
+            id: "gallery2d-spse",
+            subtitle: "SPŠE Pardubice",
+            info: "<a href='https://www.spse.cz/' target='_blank'>https://www.spse.cz/</a>",
+            images: [
+              {
+                "src": "/portfolio/2d/propagacni_materialy/m001t114_feb2122_view_04.jpg",
+                "width": 4000,
+                "height": 2250
+              },
+              {
+                "src": "/portfolio/2d/propagacni_materialy/dl.jpg",
+                "width": 2200,
+                "height": 1400
+              },
+              {
+                "src": "/portfolio/2d/propagacni_materialy/dl_leaflet_mockup_01.jpg",
+                "width": 4000,
+                "height": 3000
+              },
+              {
+                "src": "/portfolio/2d/propagacni_materialy/DL2.jpg",
+                "width": 2200,
+                "height": 1400
+              },
+            ],
+          },
+          {
+            id: "gallery2d-gamespace",
+            subtitle: "Game Space",
+            info: "Jedná se o fiktivní společnost. Je tu zde pro ukázku mých schopností.",
+            images: [
+              {
+                "src": "/portfolio/2d/gamespace/web_gamespace.png",
+                "width": 4736,
+                "height": 2659
+              },
+              {
+                "src": "/portfolio/2d/gamespace/gamespace_logo_1.jpg",
+                "width": 3000,
+                "height": 1800
+              },
+              {
+                "src": "/portfolio/2d/gamespace/gamespace_logo_2.jpg",
+                "width": 3000,
+                "height": 1800
+              },
+              {
+                "src": "/portfolio/2d/gamespace/gamespace_logo_3.jpg",
+                "width": 3962,
+                "height": 2835
+              },
+              {
+                "src": "/portfolio/2d/gamespace/gamespace_logo_4.jpg",
+                "width": 3962,
+                "height": 2835
+              },
+              {
+                "src": "/portfolio/2d/gamespace/insta_post_gamespace_1_mock.jpg",
+                "width": 2083,
+                "height": 2083
+              },
+              {
+                "src": "/portfolio/2d/gamespace/insta_post_gamespace_1_mock_up.jpg",
+                "width": 2083,
+                "height": 2083
+              },
+              {
+                "src": "/portfolio/2d/gamespace/banner_gamespace_mock_up_2.jpg",
+                "width": 5000,
+                "height": 3400
+              },
+              {
+                "src": "/portfolio/2d/gamespace/banner_gamespace_mock_up.jpg",
+                "width": 3456,
+                "height": 5184
+              },
+              {
+                "src": "/portfolio/2d/gamespace/banner_gamespace.jpg",
+                "width": 6666,
+                "height": 14999
+              },
+              {
+                "src": "/portfolio/2d/gamespace/insta_post_gamespace_1.jpg",
+                "width": 1080,
+                "height": 1080
+              },
+              {
+                "src": "/portfolio/2d/gamespace/insta_post_gamespace_2.jpg",
+                "width": 1080,
+                "height": 1080
+              },
+            ],
+          },
+          {
+            id: "gallery2d-jak-cestovat",
+            subtitle: "Jak cestovat",
+            info: "Jedná se o fiktivní společnost. Je tu zde pro ukázku mých schopností.",
+            images: [
+              {
+                "src": "/portfolio/2d/jak_cestovat/web2.jpg",
+                "width": 5000,
+                "height": 3337
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/web.jpg",
+                "width": 4736,
+                "height": 2659
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/banner_mock_up_jak_cestovat.jpg",
+                "width": 5000,
+                "height": 3000
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/insta_post.jpg",
+                "width": 3000,
+                "height": 3000
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/banner_jak_cestovat_2.jpg",
+                "width": 5000,
+                "height": 3055
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/logo_jak_cestovat_1.jpg",
+                "width": 3600,
+                "height": 2400
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/logo_jak_cestovat_3.jpg",
+                "width": 3500,
+                "height": 2100
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/insta_post_jak_cestovat_1.jpg",
+                "width": 1080,
+                "height": 1080
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/banner_jak_cestovat.jpg",
+                "width": 6666,
+                "height": 14999
+              },
+              {
+                "src": "/portfolio/2d/jak_cestovat/insta_post_jak_cestovat_2.jpg",
+                "width": 1080,
+                "height": 1080
+              },
+            ],
+          },
+          {
+            id: "gallery2d-materials",
+            subtitle: "Ostatní",
             images: [
               {
                 "src": "/portfolio/2d/propagacni_materialy/21022459.jpg",
@@ -33,19 +204,9 @@ export default defineNuxtComponent({
                 "height": 2000
               },
               {
-                "src": "/portfolio/2d/propagacni_materialy/5800894.jpg",
-                "width": 7162,
-                "height": 4780
-              },
-              {
                 "src": "/portfolio/2d/propagacni_materialy/Bella_vizitka.jpg",
                 "width": 3000,
                 "height": 2000
-              },
-              {
-                "src": "/portfolio/2d/propagacni_materialy/DL2.jpg",
-                "width": 2200,
-                "height": 1400
               },
               {
                 "src": "/portfolio/2d/propagacni_materialy/VACLAVKOVA_VERONIKA_2G_PROPAGACE_01.jpg",
@@ -83,16 +244,6 @@ export default defineNuxtComponent({
                 "height": 4480
               },
               {
-                "src": "/portfolio/2d/propagacni_materialy/dl.jpg",
-                "width": 2200,
-                "height": 1400
-              },
-              {
-                "src": "/portfolio/2d/propagacni_materialy/dl_leaflet_mockup_01.jpg",
-                "width": 4000,
-                "height": 3000
-              },
-              {
                 "src": "/portfolio/2d/propagacni_materialy/helcavizitka.jpg",
                 "width": 3200,
                 "height": 2400
@@ -101,11 +252,6 @@ export default defineNuxtComponent({
                 "src": "/portfolio/2d/propagacni_materialy/kniha.jpg",
                 "width": 3600,
                 "height": 2400
-              },
-              {
-                "src": "/portfolio/2d/propagacni_materialy/m001t114_feb2122_view_04.jpg",
-                "width": 4000,
-                "height": 2250
               },
               {
                 "src": "/portfolio/2d/propagacni_materialy/manual_loga.jpg",
